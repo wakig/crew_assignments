02e6ef8b835fc8220c1c6ea4f7be9b430ab1a005
Waki G <37522888+wakig@users.noreply.github.com>
2020-10-26 17:06:27 +0800

    include flight schedules

--- src/crew_assignments/views.py
+++ src/crew_assignments/views.py
@@ -10,7 +10,8 @@ def index(request):
 class HomeView(View):
     template_name = 'crew_assignments/home_view.html'
     crew_list = Crew.objects.all()
-    context = {'crew_list': crew_list}
+    flight_list = ScheduledFlight.objects.all()
+    context = {'crew_list': crew_list, 'flight_list': flight_list}
     
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.context)
